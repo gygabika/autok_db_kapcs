dd3b594bc1d4b8b70d607ce29b523e1ff85e6123
Gyura Gabriella 588 <gyugab588@hengersor.hu>
2025-02-26 11:06:32 +0100

    szerver futtatása

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemon = require("nodemon");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+ 
+app.get("/", (rqs, res) => {
+    res.send("Szerver a 8080-as Porton fut.")
+});
+ 
+app.listen(8080, () => {
+    console.log("A 8080 porton fut!")
+});
+
+const db = mysql.createConnection({
+        user: "root",
+        host: "localhost",
+        port: 3307,
+        password: "",
+        database: "driveus"
+});
+
+app.get("/kolcsonozhetoautok", (req, res) => {
+    const sql = "SELECT * FROM `kolcsonozhetoautok`";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+});
+
+app.get("/marka", (req,res) => {
+    const sql = "SELECT `marka` FROM `kolcsonozhetoaut`;";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+});
